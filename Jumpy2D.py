7ffa2f48980cc3b2f0b5ac2ba0060dd5d8e732ec
Dominik Schmid <DominikSchmid93@gmail.com>
2012-05-25 02:02:21 +1200

    Shifted quite a bit into the Engine class

--- Jumpy2D.py
+++ Jumpy2D.py
@@ -1,48 +1,22 @@
 import pyglet
-from pyglet.gl import *
 
-from Controller import Controller
-from Vector import Vector
+from game.Engine import Engine
+
 
 window = pyglet.window.Window()
+engine = Engine(window)
 posX = posY = 0
-c = Controller()
-windowCenter = Vector(window.get_size()[0], window.get_size()[1]) / 2
 
-# draw a cross at the position of a vector
-def drawCross(v):
-  r = 8
-  glBegin(GL_LINES)
-  glVertex2f(v.x  , v.y+r)
-  glVertex2f(v.x  , v.y-r)
-  glVertex2f(v.x+r, v.y  )
-  glVertex2f(v.x-r, v.y  )
-  glEnd()
 
 @window.event
 def on_draw():
-  global posX, posY, c, windowCenter
-  glClearColor(0,0,0, 50)
-  glClear(GL_COLOR_BUFFER_BIT)
-  glLoadIdentity()
-
-  glColor3f(1.0, 0.0, 0.0)
-  drawCross(Vector(posX, posY))
-
-  glColor3f(0.0, 1.0, 0.0)
-  drawCross(windowCenter + 100 * c.stick_l_raw)
-  glColor3f(1.0, 1.0, 0.0)
-  drawCross(windowCenter + 100 * c.stick_r_raw)
-
-  glColor3f(1.0, 1.0, 1.0)
-  drawCross(windowCenter)
+  engine.run()
 
 @window.event
 def on_mouse_motion(x, y, dx, dy):
   global posX, posY
   posX = x
   posY = y
-
 
 
 pyglet.app.run()
---
+++ game/Engine.py
@@ -0,0 +1,53 @@
+from pyglet.gl import *
+
+from game.Controller import Controller
+from game.Vector import Vector
+
+class Engine:
+
+  window = None
+  windowCenter = Vector()
+
+  gameController = None
+
+  groups = {} # TODO map from group to list of Entities
+
+  time = 0
+  prevTime = 0
+
+
+  def __init__(self, window):
+    self.window = window
+    self.windowCenter = Vector(window.get_size()[0], window.get_size()[1]) / 2
+
+    try:
+      gameController = Controller()
+    except: pass
+
+  # DEBUG: draw a cross at the position of a vector
+  def drawCross(self, v):
+    r = 8
+    glBegin(GL_LINES)
+    glVertex2f(v.x  , v.y+r)
+    glVertex2f(v.x  , v.y-r)
+    glVertex2f(v.x+r, v.y  )
+    glVertex2f(v.x-r, v.y  )
+    glEnd()
+
+
+  def run(self):
+    glClearColor(0,0,0, 0)
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+
+    glColor3f(1.0, 0.0, 0.0)
+    #self.drawCross(Vector(posX, posY)) # FIXME: mouse input
+
+    if self.gameController: # if we have a controller
+      glColor3f(0.0, 1.0, 0.0)
+      self.drawCross(self.windowCenter + 100 * c.stick_l_raw)
+      glColor3f(1.0, 1.0, 0.0)
+      self.drawCross(self.windowCenter + 100 * c.stick_r_raw)
+
+    glColor3f(1.0, 1.0, 1.0)
+    self.drawCross(self.windowCenter)
